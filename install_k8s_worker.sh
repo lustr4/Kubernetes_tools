f11a796fa080a3a112134f4f0f16793faa39b92e
lustr4 <82339981+lustr4@users.noreply.github.com>
2024-03-17 17:10:01 +0100

    Create install_k8s_worker.sh

---
+++ install_k8s_worker.sh
@@ -0,0 +1,68 @@
+########################################
+# K8s: Version 1.28 
+# Linux: Ubuntu 22.04
+# 
+# This script is Installation Kubernetes on Ubuntu with containerd 
+# as the container runtime and flannel as the CNI plugin for a worker node.
+# After the script runs successfully the node can then run the kubeadm join 
+# command that the master node outputs after kubeadm init
+# or after this command:
+# sudo kubeadm token create --print-join-command
+########################################
+# This script assumes swap is disabled #
+# sudo swapoff -a
+# sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+# Forwarding IPv4 and letting iptables see bridged traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Install containerd
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo mkdir -p /etc/containerd
+sudo bash -c 'containerd config default > /etc/containerd/config.toml'
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl enable containerd
+sudo systemctl restart containerd
+
+# Install kubeadm, kubelet and kubectl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet=1.28.0-1.1 kubeadm=1.28.0-1.1 kubectl=1.28.0-1.1
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl start kubelet
+sudo systemctl enable kubelet
