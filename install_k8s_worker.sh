cbc8d87f34ae10f15672c26d5f811fb953d38344
lustr4 <82339981+lustr4@users.noreply.github.com>
2024-03-17 18:07:58 +0100

    Update install_k8s_worker.sh
    Add -y to automate SW package installation

--- install_k8s_worker.sh
+++ install_k8s_worker.sh
@@ -31,7 +31,7 @@ EOF
 sudo sysctl --system
 
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -48,7 +48,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo mkdir -p /etc/containerd
 sudo bash -c 'containerd config default > /etc/containerd/config.toml'
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
